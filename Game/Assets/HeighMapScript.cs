e170aa39a1d25904661c2b0bab46390e968efb98
GogoLike99 <iekalayda@gmail.com>
2020-02-03 12:34:49 +0300

    Добавил LFS

---
+++ Game/Assets/HeighMapScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HeighMapScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terrain tr = GetComponent<Terrain>();
+        System.Random rand = new System.Random();
+        int z = 129;
+        float[,] a = new float[z,z];
+        for(int i = 0; i<z; i++)
+        {
+            for(int k = 0; k<z; k++)
+            {
+                a[i, k] = (float)rand.NextDouble();
+            }
+        }
+        tr.terrainData.SetHeights(0, 0, a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
