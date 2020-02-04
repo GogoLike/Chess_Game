3caaede378590ffe5227c3e21e1a54baac56c864
GogoLike99 <iekalayda@gmail.com>
2020-02-04 21:15:11 +0300

    Сделал чтение и постройку карты из файла .hmap

--- Game/Assets/CubeWorldScript.cs
+++ Game/Assets/CubeWorldScript.cs
@@ -9,7 +9,7 @@ public class CubeWorldScript : MonoBehaviour
 {
     private int[,] heightMap;
     public GameObject Cube;
-    private int Width=4;
+    private int Width;
     private int width;
     public string game_path = @"D:/Unity_Projects/Chess_Game/Game";
     public string world_name;
@@ -29,15 +29,34 @@ public class CubeWorldScript : MonoBehaviour
 
     private void ReadWorld(string name)
     {
-        string path = game_path + @"/Assets/Worlds/" + name + @".txt";
+        string path = game_path + @"/Assets/Worlds/" + name + @".hmap";
         using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
         {
             string line1;
+            int x = 0;
             while ((line1 = sr.ReadLine()) != null)
             {
-                string[] line = line1.Split('=');
-                Debug.Log(line[0]);
-                Debug.Log(line[1]);
+                string[] line2 = line1.Split('=');
+                if (line2[0] == "width")
+                {
+                    Width = Convert.ToInt32(line2[1]);
+                    if (Math.Log(Width, 2) % 1 != 0)
+                    {
+                        Debug.Log("Width is not x^2");
+                        return;
+                    }
+                    width = Width + 1;
+                    heightMap = new int[width, width];
+                }
+                else
+                {
+                    string[] line3 = line1.Split(',');
+                    for (int z = 0; z<width;z++)
+                    {
+                        heightMap[x, z] = Convert.ToInt32(line3[z]);
+                    }
+                    x++;
+                }
             }
         }
 
@@ -45,20 +64,6 @@ public class CubeWorldScript : MonoBehaviour
 
     private void GenerateWorld()
     {
-        if (Math.Log(Width, 2) % 1 != 0)
-        {
-            Debug.Log("Width is not x^2");
-            return;
-        }
-        width = Width + 1;
-        heightMap = new int[width, width];
-        for (int i1 = 0; i1 < width; i1++) 
-        {
-            for (int k1 = 0; k1 < width; k1++)
-            {
-                heightMap[i1, k1] = 1;
-            }
-        }
         for (int x = 0; x<width; x++)
         {
             for(int z = 0; z<width; z++)
